e6b68913b45015a97c0a632e38f37de9123261e6
jerzybalcer <jurekbalcer@gmail.com>
2024-11-14 17:17:33 +0100

    Create TestWallet.ts

---
+++ src/TestWallet.ts
@@ -0,0 +1,15 @@
+import { Wallet } from "./wallet/Wallet.js";
+
+
+const wallet = new Wallet('bezpieczne hasło');
+
+const keyPair = wallet.getFirstAvailableKeyPair();
+
+if(keyPair) {
+    console.log('Encrypted private key', keyPair.encryptedPrivateKey);
+    console.log('Decrypted private key', keyPair.getDecryptedPrivateKey('bezpieczne hasło'));
+    console.log('Address', keyPair.getAddress());
+}
+
+wallet.generateNewKeyPair('bezpieczne hasło');
+
